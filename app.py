8d19becc962a1948d01f9d79662537a0011e0f2a
Matthew Frost <matthewfrosty@gmail.com>
2021-12-03 21:31:27 +0000

    using sqlite

--- app.py
+++ app.py
@@ -1,66 +1,117 @@
+from sqlite3.dbapi2 import Cursor
 from tgtg import TgtgClient
 import requests
-import time
 import configparser
 import sqlite3
+import os
+
+config = configparser.ConfigParser()
+config.read("config.env")
+
+con = sqlite3.connect("base.db")
 
 class Bag:
-    def __init__(self, location, availableBags):
+    def __init__(self, store_id, location, availableBags):
+        self.store_id = store_id
         self.location = location
         self.availableBags = availableBags
     
     def __str__(self):
         return "Location: {location} \nAvailable bags: {count}\n".format(location=self.location, count=self.availableBags)
 
-con = sqlite3.connect("base.db")
-cursor = con.cursor()
+def init_db():
+    cursor = con.cursor()
 
-cursor.execute('''CREATE TABLE bags'
-                    (store_id INTEGER, store_name TEXT, available_bags INTEGER)''')
-config = configparser.ConfigParser()
-config.read("config.env")
+    cursor.execute("CREATE TABLE IF NOT EXISTS bags(store_id INTEGER PRIMARY KEY, store_name TEXT, available_bags INTEGER)")
 
-tgtgSettings = config["Too good to go"]
-telegramSettings = config["Telegram"]
-locationSettings = config["Location"]
+    return cursor
 
-client = TgtgClient(email=tgtgSettings["email"], password=tgtgSettings["password"])
-bot_token=telegramSettings["botToken"]
-chat_id=telegramSettings["chatId"]
+def init_tgtg():
+    if not "access_token" in os.enivron:
+        client = TgtgClient(email=os.environ["email"])
+        client.get_credentials()
+    else:
+        client = TgtgClient(access_token=os.enivron["access_token"], refresh_token=os.enivron["refresh_token"], user_id=os.enivron["user_id"])
+    
+    os.enivron["access_token"] = client.access_token
+    os.enivron["refresh_token"] = client.refresh_token
+    os.enivron["user_id"] = client.user_id
+    # with open("config.env", "w") as configFile:
+    #     config.write(configFile)
+    return client
 
-sentObjects = []
-while True: 
-    print("Getting bags")
-    availableObjects = []
-    objectsToSend = [] 
+def get_saved_bags(cursor):
+    savedBags = []
+    for row in cursor.execute("SELECT * FROM bags"):
+        print(row)
+        savedBags.append(Bag(row[0], row[1], row[2]))
+    return savedBags
 
-    response = client.get_items(
+#Compares the bags in the database with the ones retrieve from the API
+#If a bag exists in the database but not in the API results then it is removed
+def remove_old_bags(cursor, saved_bags, new_bags):
+    bagsToRemove = []
+    for bag in saved_bags:
+        #if a saved bag does not exist in the new bags then it must not be available anymore
+        if not any(element.location == bag.location for element in new_bags):
+            bagsToRemove.append(bag)
+    
+    for bag in bagsToRemove:
+        cursor.execute("DELETE FROM bags WHERE store_name = ?", [bag.location])
+    
+    con.commit()
+
+    return list(set(bagsToRemove).difference(set(saved_bags)))
+
+def get_new_bags(tgtgClient):
+    response = tgtgClient.get_items(
         favorites_only=True,
-        latitude=locationSettings["latitude"],
-        longitude=locationSettings["longitude"],
-        radius=locationSettings["radius"],
+        latitude=os.environ["latitude"],
+        longitude=os.environ["longitude"],
+        radius=os.environ["radius"],
     )
 
+    newBags = []
     #reading the response and adding to list
     for x in response:
-        storeText = ""
         if x["items_available"] != 0:
-            #storeText += "Location: {location} \n".format(location=x["display_name"])
-            #storeText += "Available bags: {items} \n".format(items=x["items_available"])
-            #availableLocations.append(storeText)
-            availableObjects.append(Bag(x["display_name"], x["items_available"]))
-      
-    for bag in availableObjects:
-        if not any(element.location == bag.location for element in sentObjects):
-            sentObjects.append(bag)
-            objectsToSend.append(bag)
+            cursor.execute("INSERT OR REPLACE INTO bags (store_id, store_name, available_bags) VALUES(?, ?, ?)", [x["store"]["store_id"], x["display_name"], x["items_available"]])
+            newBags.append(Bag(x["store"]["store_id"], x["display_name"], x["items_available"]))
+        con.commit()
+    
+    return newBags
 
+def send_telegram_message(bags):
+    telegramSettings = config["Telegram"]
+    bot_token=os.environ["bottoken"]
+    chat_id=os.environ["chatid"]
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     params = {
         "chat_id": chat_id,
-        "text": "\n".join(str(x) for x in objectsToSend)
+        "text": "\n".join(str(x) for x in bags)
     }
 
     resp = requests.get(url, params=params)
 
-    time.sleep(300)
+
+
+
+cursor = init_db()
+
+#tgtgSettings = config["Too good to go"]
+
+#locationSettings = config["Location"]
+
+client = init_tgtg()
+
+
+
+print("Getting bags")
+
+objectsToSend = [] 
+
+newBags = get_new_bags(client)
+existingBags = get_saved_bags(cursor)
+newBags = remove_old_bags(cursor, existingBags, newBags)
+
+send_telegram_message(newBags)
